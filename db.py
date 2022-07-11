3509c3ff91485d45f557944d307e1715fded8add
John Nikolas Boyle <nikolasboyle@gmail.com>
2022-07-11 07:11:31 -0700

    Initial commit

---
+++ db.py
@@ -0,0 +1,43 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Column, BigInteger, Text, Date
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import database_exists, create_database
+
+# Postgres database configuration
+pg_config = {
+    'user': 'johnboyle',
+    'password': '#Trinity13',
+    'host': '127.0.0.1',
+    'port': '5432',
+    'db': 'job_scraper'
+}
+
+# Postgres database connection url
+url = f"postgresql://{pg_config['user']}:{pg_config['password']}@{pg_config['host']}:{pg_config['port']}/{pg_config['db']}"
+
+# Create the database if it does not exist
+if not database_exists(url):
+    create_database(url)
+
+# Create engine, session and base
+engine = create_engine(url, pool_size=50, echo=False)
+session = sessionmaker(bind=engine)()
+Base = declarative_base()
+
+# Create job table
+class Job(Base):
+    __tablename__ = 'job'
+
+    id = Column(BigInteger, primary_key=True)
+    title = Column(Text, nullable=False)
+    company = Column(Text, nullable=False)
+    location = Column(Text, nullable=False)
+    description = Column(Text, nullable=False)
+    link = Column(Text, nullable=False)
+    date = Column(Date, nullable=False)
+
+    def __repr__(self):
+        return f"User(id={self.id!r}, title={self.title!r}, company={self.company!r}, location={self.location!r}, description={self.description!r}, link={self.link!r}, date={self.date!r})"
+
+Base.metadata.create_all(engine)
---
+++ match.py
@@ -0,0 +1,24 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import docx2txt
+import re
+
+from db import session, Job
+
+# Get cleaned resume text
+resume = " ".join(str(docx2txt.process("resume.docx")).split())
+
+# Get all jobs
+jobs = session.query(Job).all()
+
+# Calculate match similarity between job description and resume
+cv = CountVectorizer()
+
+for job in jobs:
+
+    description = re.sub(re.compile('<.*?>'), '', job.description)
+
+    count_matrix = cv.fit_transform([resume, description])
+    match_percentage = round(cosine_similarity(count_matrix)[0][1] * 100, 2)
+    
+    print(match_percentage)
